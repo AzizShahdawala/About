92aa4804cefb4937abf05d624fa770360605d693
Aziz <azizshada@gmail.com>
2020-05-08 01:10:54 +0530

    Completed Home Page

---
+++ app.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.set(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.listen(3000, function(req,res){
+    console.log("Sever started on port 3000");
+});
