06920965516be7be9ebc82e2d77d824931e8b112
Aziz <azizshada@gmail.com>
2020-05-08 16:44:41 +0530

    Added PORT to app.listen

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/", function(req,res){
     res.render("home");
 });
 
-app.listen(3000, function(req,res){
+app.listen(process.env.PORT || 3000, function(req,res){
     console.log("Sever started on port 3000");
 });
